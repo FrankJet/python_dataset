b33ef73f0d4bb9aeb5fca5ab9527a8c28d679f87
FrankJet <jetfish1900@163.com>
2014-10-21 14:27:22 +0800

    coef_cdf
    new function:
    1.add new function get_histogram
    2.add float

--- LR/coef_CDF.py
+++ LR/coef_CDF.py
@@ -6,7 +6,7 @@ import math
 import matplotlib.pyplot as plt
 
 def readTxtToList():
-	fp_read = open("readTest.txt", "r")
+	fp_read = open("coef_result.txt", "r")
 	coefList = []
 	
 	for line in fp_read:
@@ -18,13 +18,12 @@ def readTxtToList():
 	return coefList
 
 def mainFunction():
-	
 	coefList = readTxtToList()
-
 	(keyValue, keyPercentile) = statisticAttribute(coefList)
 	print keyValue, keyPercentile
-	(cdf_x, cdf_y)=get_CDF(coefList)
-	plot_result(cdf_x, cdf_y, keyValue, keyPercentile)
+	get_histogram(coefList)
+	#(cdf_x, cdf_y)=get_CDF(coefList)
+	#plot_result(cdf_x, cdf_y, keyValue, keyPercentile)
 	
 def statisticAttribute(numList):
 	maxNum = max(numList)
@@ -34,26 +33,39 @@ def statisticAttribute(numList):
 
 	varNum = np.var(numList) 
 	
-	print "max value %d min value %d mean %d var %f"%(maxNum, minNum, meanNum, varNum)
+	print "max value %f min value %f mean %f var %f"%(maxNum, minNum, meanNum, varNum)
 	
-	keyPercentile = 90
+	keyPercentile = 50
 	keyValue = np.percentile(numList, keyPercentile)
 	return (keyValue, float(keyPercentile)/100)	
 	
 def get_CDF(numList):
 	print "total number of numList %d"%len(numList)
 	numArray = np.asarray(numList)
-	bins_num = np.arange(np.floor(numArray.min()), np.ceil(numArray.max()))
+	bins_num = 30
+	#bins_num = np.arange(np.floor(numArray.min()), np.ceil(numArray.max()))
+	print np.floor(numArray.min()), np.ceil(numArray.max())
 	hist, bin_edges = np.histogram(numArray, bins=bins_num, density=True)
 	#print hist
 	cdf = np.cumsum(hist)
 	
 	return (bin_edges[1:], cdf)
 
+def get_histogram(numList):
+	print "total number of numList %d"%len(numList)
+	numArray = np.asarray(numList)
+	bins_num = 30
+	plt.hist(numArray, bins=bins_num)
+	plt.show()
+	#hist, bin_edges = np.histogram(numArray, bins=bins_num, density=True)
+	#print hist
+	#cdf = np.cumsum(hist)
+	
+	#return (bin_edges[1:], cdf)
+	
 def plot_result(xArray, yArray, keyValue, keyPercentile):
 	plt.plot(xArray, yArray)
 	plt.plot([keyValue], [keyPercentile], 'ro')
 	plt.show()	
 
-	
-readTxtToList()
+mainFunction()
